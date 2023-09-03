d08938d65ffab4e05afe56f9c64e236e316c037b
Biel1895 <gabrielmlrrios@gmail.com>
2023-09-03 16:21:53 -0300

    prjeto finalizado

---
+++ src/index.js
@@ -0,0 +1,28 @@
+let nickname = "El Ruivo"
+let XP = 10000
+
+    if(XP <= 1000){
+        XP = "Ferro"
+    }
+        else if(XP > 1000 && XP <= 2000){
+        XP = "Bronze"
+    }
+        else if(XP > 2000 && XP <= 5000){
+        XP = "Prata"
+    }
+        else if(XP > 5000 && XP <= 7000){
+        XP = "Ouro"
+    }
+        else if(XP > 7000 && XP <= 8000){
+        XP = "Platina"
+    }
+        else if(XP > 8000 && XP <= 9000){
+        XP = "Ascendente"
+    }
+        else if(XP > 9000 && XP <= 10000){
+        XP = "Imortal"
+    }
+        else if(XP > 10000){
+        XP = "Radiante"
+    }
+        console.log ("O Héroi de nome " + nickname + ", está no nível " + XP)
